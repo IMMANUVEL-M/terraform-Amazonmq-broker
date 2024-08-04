40523916278b910bc1945d2667d8c50b40e3780b
Immanuvel-M <51225081+IMMANUVEL-M@users.noreply.github.com>
2024-08-04 18:42:47 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -10,7 +10,7 @@ locals {
   mq_broker_deployment_mode            = "SINGLE_INSTANCE"
   mq_broker_engine_type                = "ActiveMQ"
   mq_broker_engine_version             = "5.17.6"
-  mq_broker_host_instance_type         = "mq.m5.large" #Choosed the closest matching Size
+  mq_broker_host_instance_type         = "mq.m5.large" #You can Replace the instance type here as per your requirement 
   mq_broker_publicly_accessible        = false
   mq_broker_general_log_enabled        = true
   mq_broker_audit_log_enabled          = true
@@ -20,7 +20,7 @@ locals {
   mq_broker_maintenance_time_of_day    = "" #You can provide  your preferred time of the day
   mq_broker_maintenance_time_zone      = "" #You can provide  your preferred time_zone
   mq_broker_vpc_id                     = data.aws_vpc.vpc_details.id
-  mq_broker_subnet_ids                 = [data.aws_subnet.webapp_subnet_a.id] #this value will be depends_onon deployment mode
+  mq_broker_subnet_ids                 = [data.aws_subnet.webapp_subnet_a.id] #this value will be depends_on deployment mode
   mq_broker_associated_security_group_ids = data.aws_security_groups.associated.ids
   secret_data                          = jsondecode(data.aws_secretsmanager_secret_version.secret_details.secret_string)
   mq_broker_admin_username             = local.secret_data["admin_user_name"]
